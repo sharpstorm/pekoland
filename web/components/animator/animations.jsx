9292d6b967d239c8d72c5463012dd1e9b3305ad7
Ho Zong Han <chimchar17@gmail.com>
2021-05-21 18:06:54 +0800

    Added Slide Up Animation to Animator

--- web/components/animator/animations.jsx
+++ web/components/animator/animations.jsx
@@ -52,4 +52,55 @@ class AnimSlideOut extends React.Component {
   }
 }
 
-export { AnimSlideOut }
+class AnimSlideUp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      childPosition: SlideAnimator.CENTER,
+      curChild: props.children,
+      curUniqId: props.uniqId,
+      prevChild: null,
+      prevUniqId: null,
+      animationCallback: null
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const prevUniqId = prevProps.uniqKey || prevProps.children.type;
+    const uniqId = this.props.uniqKey || this.props.children.type;
+
+    if (prevUniqId !== uniqId) {
+      this.setState({
+        childPosition: SlideAnimator.TO_BOTTOM,
+        curChild: this.props.children,
+        curUniqId: uniqId,
+        prevChild: prevProps.children,
+        prevUniqId,
+        animationCallback: this.swapChildren
+      });
+    }
+  }
+
+  swapChildren = () => {
+    this.setState({
+      childPosition: SlideAnimator.FROM_TOP,
+      prevChild: null,
+      prevUniqId: null,
+      animationCallback: null
+    });
+  };
+
+  render() {
+    return (
+      <SlideAnimator
+        position={this.state.childPosition}
+        animationCallback={this.state.animationCallback}
+      >
+        {this.state.prevChild || this.state.curChild}
+      </SlideAnimator>
+    );
+  }
+}
+
+export { AnimSlideOut, AnimSlideUp }
