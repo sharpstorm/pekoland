0945c4b5336f4571f842b2cc91bf7f1407e027d8
Ho Zong Han <chimchar17@gmail.com>
2021-06-01 16:46:55 +0800

    Reorganise Networking Senders and Handlers

--- game/net/client/game-data-handler.js
+++ game/net/client/game-data-handler.js
@@ -4,46 +4,66 @@ import Player from '../../models/player.js';
 import NetworkManager from '../network-manager.js';
 import buildClientGamePacket from './game-data-sender.js';
 
+const handlers = {
+  'handshake': handleHandshake,
+  'spawn-reply': handleSpawnReply,
+  'spawn-reject': handleSpawnReject,
+  'spawn-player': handleSpawnPlayer,
+  'despawn-player': handleDespawnPlayer,
+  'move-echo': handleMoveEcho,
+  'chat-echo': handleChatEcho
+};
+
 // Conn will always be the server
 export default function handleGamePacket(data, conn) {
   console.log(data);
   if (!data.opCode) return;
 
   const opCode = data.opCode;
-  if (opCode === 'handshake') {
-    conn.send(buildClientGamePacket('spawn-request', {
-      name: NetworkManager.getInstance().configStore.name,
-      userId: NetworkManager.getInstance().configStore.userId
-    }));
-  
-  } else if (opCode === 'spawn-reply') {
-    let self = inflatePlayer(data.self);
-    PlayerManager.getInstance().addPlayer(self);
-    PlayerManager.getInstance().setSelf(self.userId);
-    data.others.forEach(x => {
-      PlayerManager.getInstance().addPlayer(inflatePlayer(x));
-    });
-  
-  } else if (opCode === 'spawn-reject') {
-    alert(data.msg);
-    window.close();
-  
-  } else if (opCode === 'spawn-player') {
-    PlayerManager.getInstance().addPlayer(inflatePlayer(data.player));
-  
-  } else if (opCode === 'despawn-player') {
-    console.log('de spawn ' + data.userId);
-    PlayerManager.getInstance().removePlayer(data.userId);
-  
-  } else if (opCode === 'move-echo') {
-    let player = PlayerManager.getInstance().getPlayer(data.userId);
-    player.moveTo(data.x, data.y);
-    player.direction = data.direction;
-
-  } else if (opCode === 'chat-echo') {
-    let player = PlayerManager.getInstance().getPlayer(data.userId);
-    player.chat.updateMessage(data.message);
+  if (opCode in handlers) {
+    return handlers[opCode](data, conn);
   }
+  console.log('[ClientHandler] Unknown Op Code: ' + opCode);
+}
+
+function handleHandshake(data, conn) {
+  conn.send(buildClientGamePacket('spawn-request', {
+    name: NetworkManager.getInstance().configStore.name,
+    userId: NetworkManager.getInstance().configStore.userId
+  }));
+}
+
+function handleSpawnReply(data, conn) {
+  let self = inflatePlayer(data.self);
+  PlayerManager.getInstance().addPlayer(self);
+  PlayerManager.getInstance().setSelf(self.userId);
+  data.others.forEach(x => {
+    PlayerManager.getInstance().addPlayer(inflatePlayer(x));
+  });
+}
+
+function handleSpawnReject(data, conn) {
+  alert(data.msg);
+  window.close();
+}
+
+function handleSpawnPlayer(data, conn) {
+  PlayerManager.getInstance().addPlayer(inflatePlayer(data.player));
+}
+
+function handleDespawnPlayer(data, conn) {
+  PlayerManager.getInstance().removePlayer(data.userId);
+}
+
+function handleMoveEcho(data, conn) {
+  let player = PlayerManager.getInstance().getPlayer(data.userId);
+  player.moveTo(data.x, data.y);
+  player.direction = data.direction;
+}
+
+function handleChatEcho(data, conn) {
+  let player = PlayerManager.getInstance().getPlayer(data.userId);
+  player.chat.updateMessage(data.message);
 }
 
 function inflatePlayer(data) {
--- game/net/server/game-data-sender.js
+++ game/net/server/game-data-sender.js
@@ -1,50 +1,70 @@
+const handlers = {
+  'handshake': buildEmptyPacket,
+  'spawn-reply': buildSpawnReply,
+  'spawn-reject': buildSpawnReject,
+  'spawn-player': buildSpawnPlayer,
+  'despawn-player': buildDespawnPlayer,
+  'move-echo': buildMoveEcho,
+  'chat-echo': buildChatEcho
+};
+
 export default function buildGamePacket(opCode, data) {
-  if (opCode === 'handshake') {
-    return {
-      opCode
-    };
-  } else if (opCode === 'movement') {
-    return {
-      opCode,
-      data
-    };
-  } else if (opCode === 'spawn-reply') {
-    return {
-      opCode,
-      self: flattenPlayer(data.self),
-      others: data.others.map(x => flattenPlayer(x))
-    };
-  } else if (opCode === 'spawn-reject') {
-    return {
-      opCode,
-      msg: data
-    }
-  } else if (opCode === 'spawn-player') {
-    return {
-      opCode,
-      player: flattenPlayer(data)
-    };
-  } else if (opCode === 'despawn-player') {
-    return {
-      opCode,
-      userId: data
-    }
-  } else if (opCode === 'move-echo') {
-    return {
-      opCode,
-      userId: data.userId,
-      x: data.x,
-      y: data.y,
-      direction: data.direction
-    };
+  if (opCode in handlers) {
+    return handlers[opCode](opCode, data);
   }
-  else if (opCode === 'chat-echo'){
-    return{
-      opCode,
-      userId: data.userId,
-      message: data.message,
-    };
+  console.log('[ServerSender] Unknown Op Code: ' + opCode);
+  return undefined;
+}
+
+function buildEmptyPacket(opCode) {
+  return { opCode };
+}
+
+function buildSpawnReply(opCode, data) {
+  return {
+    opCode,
+    self: flattenPlayer(data.self),
+    others: data.others.map(x => flattenPlayer(x))
+  };
+}
+
+function buildSpawnReject(opCode, data) {
+  return {
+    opCode,
+    msg: data
+  };
+}
+
+function buildSpawnPlayer(opCode, data) {
+  return {
+    opCode,
+    player: flattenPlayer(data)
+  };
+}
+
+function buildDespawnPlayer(opCode, data) {
+  return {
+    opCode,
+    userId: data
   }
+}
+
+function buildMoveEcho(opCode, data) {
+  return {
+    opCode,
+    userId: data.userId,
+    x: data.x,
+    y: data.y,
+    direction: data.direction
+  };
+}
+
+function buildChatEcho(opCode, data) {
+  return{
+    opCode,
+    userId: data.userId,
+    message: data.message,
+  };
 }
 
 function flattenPlayer(playerObj) {
