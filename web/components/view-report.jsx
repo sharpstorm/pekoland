7b92f034229ee1810b4d5f005c16f13929370cb3
Ho Zong Han <chimchar17@gmail.com>
2021-06-14 20:39:02 +0800

    Add skeleton for all the feature pages

---
+++ web/components/view-report.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+// eslint-disable-next-line import/no-unresolved
+import { useIdentityContext } from 'react-netlify-identity-auth';
+
+export default function ReportView() {
+  const identity = useIdentityContext();
+  const history = useHistory();
+
+  useEffect(() => {
+    if (identity.ready && !identity.user) {
+      history.replace('/login');
+    }
+  }, [identity.ready]);
+
+  return (
+    <>
+      <div className="panel panel-dark flexbox flex-col" style={{ textAlign: 'center', paddingBottom: '16px' }}>
+        Report Page
+      </div>
+    </>
+  );
+}
