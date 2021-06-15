bc1a70133dc23d3618cdb08968b2c817beab8a65
Ho Zong Han <chimchar17@gmail.com>
2021-06-15 13:56:01 +0800

    Add Search Endpoint

---
+++ server/functions/friends-search.js
@@ -0,0 +1,72 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function handle(event, context) {
+  const { identity, user } = context.clientContext;
+
+  // Validation Step
+  if (!identity || !user) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ message: 'Unauthorized' }),
+    };
+  }
+
+  if (event.httpMethod !== 'POST' || !event.body) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Bad Request' }),
+    };
+  }
+
+  let data;
+  try {
+    data = JSON.parse(event.body);
+  } catch {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Bad Request' }),
+    };
+  }
+
+  if (data.email === undefined) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Bad Request' }),
+    };
+  }
+
+  // Validate OK
+  try {
+    const usersUrl = `${identity.url}/admin/users`;
+    const adminAuthHeader = `Bearer ${identity.token}`;
+    const userList = await fetch(usersUrl, {
+      method: 'GET',
+      headers: { Authorization: adminAuthHeader },
+    }).then((x) => x.json());
+
+    const { users } = userList;
+    let obj = users.filter((x) => x.email === data.email);
+    if (obj.length === 0) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: 'User not Found' }),
+      };
+    }
+
+    [obj] = obj;
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        email: obj.email,
+        ign: obj.user_metadata.ign,
+      }),
+    };
+  } catch (ex) {
+    console.log(ex);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Internal Error' }),
+    };
+  }
+};
