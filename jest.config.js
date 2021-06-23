49eae298685bb40bcb9a55ec937b2e52dcd77f13
Ho Zong Han <chimchar17@gmail.com>
2021-06-23 16:03:38 +0800

    Prepare Environment for ReactJS Testing

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['./game/test/setup.js']
+  testMatch: ['**/game/test/*.test.js', '**/web/test/*.test.js'],
+  setupFilesAfterEnv: ['./game/test/setup.js'],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': '<rootDir>/web/test/styleMock.js',
+    '\\.(gif|ttf|eot|svg|png|jpg|jpeg)$': '<rootDir>/web/test/fileMock.js',
+  },
 };
